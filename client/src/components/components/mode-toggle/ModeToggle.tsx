23e5ca390d80c89725fdfd7ad6446341381459e0
jespy2 <jespy2@gmail.com>
2023-12-26 10:12:35 -0700

    dark mode logic bug fixed

--- client/src/components/components/mode-toggle/ModeToggle.tsx
+++ client/src/components/components/mode-toggle/ModeToggle.tsx
@@ -4,30 +4,36 @@ import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 import { Tooltip } from "../../index";
 
 export const ModeToggle = () => {
-	const [darkMode, setDarkMode] = useState(false);
+	const [darkMode, setDarkMode] = useState(
+		localStorage.getItem("color-theme")
+			? localStorage.getItem("color-theme")
+			: 'light'
+	);
 
 	const handleToggle = (e: { preventDefault: () => void }) => {
 		e.preventDefault();
-		setDarkMode(!darkMode);
-		if (darkMode) {
-			document.documentElement.classList.remove("dark");
-			localStorage.setItem("storial-darkmode", "light");
-		} else {
-			document.documentElement.classList.add("dark");
-			localStorage.setItem("storial-darkmode", "dark");
-		}
+		setDarkMode(darkMode === "dark" ? "light" : "dark");
 	};
 
 	useEffect(() => {
-		setDarkMode(localStorage.getItem("storial-darkmode") === "dark");
-	}, []);
+		localStorage.setItem('color-theme', darkMode ? 'dark' : 'light');
+		if (darkMode === "dark") {
+			document.documentElement.classList.remove("dark");
+			document.documentElement.classList.add("light");
+			localStorage.setItem("color-theme", "light");
+		} else {
+			document.documentElement.classList.remove("light");
+			document.documentElement.classList.add("dark");
+			localStorage.setItem("color-theme", "dark");
+		}
+	}, [darkMode]);
 
 	return (
 		<div className='mode-toggle group'>
 			<Tooltip message='Toggle dark/light mode' parent='mode-toggle' />
-			<button onClick={(e) => handleToggle(e)}>
-				<MoonIcon className={`toggle-dark-icon ${darkMode && "hidden"}`} />
-				<SunIcon className={`toggle-light-icon ${!darkMode && "hidden"}`} />
+			<button onClick={(e) => handleToggle(e)} >
+				<MoonIcon className={`toggle-dark-icon ${darkMode === 'light' && "hidden"}`} />
+				<SunIcon className={`toggle-light-icon ${darkMode === 'dark' && "hidden"}`} />
 			</button>
 		</div>
 	);
